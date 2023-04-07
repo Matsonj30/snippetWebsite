5d7f2f792931564a1bbce0edf5f0ea79cd7620d6
Matsonj30 <matsonj2013@gmail.com>
2023-04-06 19:40:37 -0600

    testimonial smh

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,52 @@ function showForm(){
     form.style.display = "block"
 }
 
-function burgerMenu(){
-    var checkbox = document.getElementById("hamburgerCheckbox");
-    var menu = document.getElementById("menu");
-    var burgerMenu = document.getElementsByClassName("hamburgerMenu");
 
-    if(checkbox.checked == true){
-        menu.style.transform = "none";
-        burgerMenu[0].style.position = "fixed";
+
+
+
+
+//scroll events still trigger on button clicks, not manual scrolling (any scrolling of any event)
+document.getElementById("scrollContainer").addEventListener("scroll", scrollEvent);
+
+function changeTestimonial(direction){
+    var currentTestimonial = getCurrentTestimonial()
+    //current spot +- 1 depending on button clicked
+    nextIndex = currentTestimonial + direction;
+    if(nextIndex == -1){
+        nextIndex = 2;
     }
-    else{
-        menu.style.transform = "translate(200px)";
-        burgerMenu[0].style.position = "absolute";
+    else if(nextIndex == 3){
+        nextIndex = 0;
     }
+
+    var nextTestimonial = document.getElementById("test"+ (nextIndex)); 
+    nextTestimonial.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+}
+function getCurrentTestimonial(){
+    scrollContainer = document.getElementById("scrollContainer");
+    var testimonials = scrollContainer.getElementsByClassName("testimonial");
+
+    var positionInContainer = scrollContainer.scrollLeft;
+    var widthOfTestimonial = testimonials[0].offsetWidth; //all same width
+    var currentTestimonial = Math.floor(positionInContainer / widthOfTestimonial);
+    return currentTestimonial;
+}
+
+//cant put this in the scroll event listener for some reason
+function scrollEvent(){
+    changeCircleColor(getCurrentTestimonial())
+}
+function changeCircleColor(currentTestimonial){
+    for(i = 0; i < 3; i+=1){
+        var circle = document.getElementById("circle"+i)
+        if(currentTestimonial == i){
+            circle.style.backgroundColor = "#fafafa"
+        }
+        else{
+            circle.style.backgroundColor = "#bdbdbd"
+        }
+    }
+
 }
